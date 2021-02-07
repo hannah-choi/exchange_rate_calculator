e95af188a463190a0131558a9f1526d6e2969fee
hannah-developer <hannahchoi.developer@gmail.com>
2021-02-07 22:46:53 +0000

    add error message when not supported + showRate

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const $baseCurrency = document.getElementById("baseCurrency");
 const $targetCurrency = document.getElementById("targetCurrency");
 const swap = document.getElementById("swap");
-let baseAmount = 1;
-let targetAmount = 1;
+const baseOptions = $baseCurrency.querySelectorAll("option");
+const message = document.getElementById("message");
+const error = document.getElementById("error");
 
 const baseResult = document.querySelector(".base.number");
 const targetResult = document.querySelector(".target.number");
@@ -11,31 +12,40 @@ function calculate() {
     const baseCurrency = $baseCurrency.value;
     const targetCurrency = $targetCurrency.value;
 
-    fetch(`https://api.exchangeratesapi.io/latest?base=${currency_one}`)
+    fetch(`https://api.exchangeratesapi.io/latest?base=${baseCurrency}`)
         .then(res => res.json())
         .then(result => {
-            //const rates = result.conversion_rates[targetCurrency];
-            baseResult.value =
-                result.conversion_rates[baseCurrency] * baseAmount;
-            targetResult.value =
-                result.conversion_rates[targetCurrency] * targetAmount;
+            if (!result.rates) {
+                if (!error.classList.contains("error")) {
+                    error.classList.add("error");
+                }
+                error.classList.remove("error");
+                setTimeout(() => {
+                    error.classList.add("error");
+                }, 3000);
+                $baseCurrency.value = $baseCurrency.getAttribute(
+                    "data-previousvalue"
+                );
+            } else {
+                $baseCurrency.setAttribute("data-previousvalue", baseCurrency);
+                const rate =
+                    result.rates[targetCurrency] > 0.0001
+                        ? result.rates[targetCurrency].toFixed(4)
+                        : result.rates[targetCurrency];
+                const showRate = `1 ${baseCurrency} = ${rate} ${targetCurrency}`;
+                message.innerHTML = showRate;
+                targetResult.value = baseResult.value * rate;
+            }
         });
 }
 
-document.addEventListener("change", ({ target }) => {
-    if (target.tagName !== "SELECT") {
-        return;
-    }
-    calculate();
-});
+$baseCurrency.addEventListener("change", calculate);
+$targetCurrency.addEventListener("change", calculate);
 
 document.addEventListener("input", ({ target }) => {
-    if (target.tagName !== "INPUT") {
-        return;
+    if (target.tagName === "INPUT") {
+        calculate();
     }
-    baseAmount = baseResult.value;
-    targetAmount = targetResult.value;
-    calculate();
 });
 
 swap.addEventListener("click", () => {
