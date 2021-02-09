fd32b0b458e70a68db64bd04b970ebd725796eaa
hannah-developer <hannahchoi.developer@gmail.com>
2021-02-09 23:29:49 +0000

    removed unnecessary part of fetch

--- script.js
+++ script.js
@@ -15,27 +15,13 @@ function calculate() {
     fetch(`https://api.exchangeratesapi.io/latest?base=${baseCurrency}`)
         .then(res => res.json())
         .then(result => {
-            if (!result.rates) {
-                if (!error.classList.contains("error")) {
-                    error.classList.add("error");
-                }
-                error.classList.remove("error");
-                setTimeout(() => {
-                    error.classList.add("error");
-                }, 3000);
-                $baseCurrency.value = $baseCurrency.getAttribute(
-                    "data-previousvalue"
-                );
-            } else {
-                $baseCurrency.setAttribute("data-previousvalue", baseCurrency);
-                const rate =
-                    result.rates[targetCurrency] > 0.0001
-                        ? result.rates[targetCurrency].toFixed(4)
-                        : result.rates[targetCurrency];
-                const showRate = `1 ${baseCurrency} = ${rate} ${targetCurrency}`;
-                message.innerHTML = showRate;
-                targetResult.value = baseResult.value * rate;
-            }
+            const rate =
+                result.rates[targetCurrency] > 0.0001
+                    ? result.rates[targetCurrency].toFixed(4)
+                    : result.rates[targetCurrency];
+            const showRate = `1 ${baseCurrency} = ${rate} ${targetCurrency}`;
+            message.innerHTML = showRate;
+            targetResult.value = baseResult.value * rate;
         });
 }
 
